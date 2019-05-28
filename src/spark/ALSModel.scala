9195f8c8435347ed0b9a1660adb26101f08b83db
Administrator <2917042249@qq.com>
2019-05-28 11:41:11 +0800

    ALS算法

--- src/spark/ALSModel.scala
+++ src/spark/ALSModel.scala
@@ -3,7 +3,37 @@ import org.apache.spark.{SparkContext,SparkConf}
 import org.apache.spark.mllib.recommendation.{MatrixFactorizationModel,ALS,Rating}
 import org.apache.spark.rdd.RDD
 object ALSModel {
-      val conf = new SparkConf().setAppName("ALSModel")
-      val sc = new SparkContext(conf)
+
+      def main(args: Array[String]): Unit = {
+            val conf = new SparkConf().setAppName("ALSModel").setMaster("local")
+            val sc = new SparkContext(conf)
+            val file = sc.textFile("./input/BX-Book-Ratings.csv")
+//            val test_file = sc.textFile("./input/test.csv")
+//            val test_header = test_file.first()
+//            val test_ratingRDD = test_file.filter(row=>row != test_header).map{_.split(";") match {
+//                  case Array(user,item,rate) =>Rating(user.toInt,item.toInt,rate.toDouble)
+//            }}
+            val header = file.first()
+            val Train_ratingRDD = file.filter(row=>row != header).map{_.split(";") match {
+                  case Array(user,item,rate) =>Rating(user.replaceAll("\"","").toInt,item.replace("X","").replaceAll("\"","").toInt,rate.replaceAll("\"","").toDouble)
+            }}
+            val rank = 10
+            val numIterations = 10
+            val alsModel = ALS.train(Train_ratingRDD,rank,numIterations,0.01)
+            val user_product = Train_ratingRDD.map{case Rating(user,product,rate)=>(user,product)}
+            val predictions = alsModel.predict(user_product).map{case Rating(user,product,rate)=>((user,product),rate)}
+            val ratesAndPredictions = Train_ratingRDD.map{case Rating(user,product,rate)=>((user,product),rate)}.join(predictions)
+            val MSE = ratesAndPredictions.map{case ((user,product),(r1,r2))=>
+                  val err = (r1 - r2)
+                  err * err
+            }.mean()
+            println("Mean Squared Error = "+MSE)
+            println("User"+"\t"+"Products"+"\t"+"Rate"+"\t"+"Prediction")
+            ratesAndPredictions.collect().foreach(
+                  rating => {
+                        println(rating._1._1+"\t"+rating._1._2+"\t"+rating._2._1+"\t"+rating._2._2)
+                  }
+            )
+      }
 
 }
